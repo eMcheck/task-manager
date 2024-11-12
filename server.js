648065bb3bb9426176faf5282e26558d6b861dff
eMcheck <vladmil.mj@gmail.com>
2024-11-12 16:47:47 +0300

    add expess with body-parser

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-const sum = (a, b) => a + b;
+const express = require('express');
+const bodyParser = require('body-parser');
 
-console.log(sum(10, 5));
+const app = express();
+const port = 3000;
+
+//middleware
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`Server listenning on port ${port} and starting at http://localhost:${port}`);
+})
